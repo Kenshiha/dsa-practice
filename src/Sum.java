679d60e504ad8bee7fcecf9ead0d290e03763df0
kenshiha <kyokasuigetsu979@gmail.com>
2025-12-07 22:41:41 +0530

    Two Sum Question Add

---
+++ src/Sum.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sum {
+
+    public int[] twoSum(int[] arr,int target){
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length;i++ ) {
+            int need = target - arr[i];
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+        map.put(arr[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1,2,4,5,6,7,8};
+        int target = 9;
+
+        Sum s = new Sum();
+        int[] result = s.twoSum(arr, target);
+
+        if (result.length == 2){
+            System.out.println("Values = " + arr[result[0]] + " , " + arr[result[1]]);
+        }else{
+            System.out.println("No Pair Found");
+        }
+
+    }
+}
