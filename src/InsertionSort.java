0f40b950feb3a6fac2daf55bd93bd15e31cb5d64
kenshiha <kyokasuigetsu979@gmail.com>
2025-12-09 23:07:59 +0530

    Insertion Sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr){
+        for (int i=1; i < arr.length;i++){
+            int current = arr[i];
+            int j = i-1;
+            while (j >=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {2,1,4,8,5,7,6,9};
+        System.out.println("Before Sorting = "+ Arrays.toString(arr));
+
+        insertionSort(arr);
+        System.out.println("After Sorting = "+ Arrays.toString(arr));
+    }
+}
