c31b31299944e113088308fa4f75c2cc93c84d4a
kenshiha <kyokasuigetsu979@gmail.com>
2025-12-13 23:50:14 +0530

    Prefix sum add

---
+++ src/arrays/PrefixSum.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class PrefixSum {
+    public static int[] buildPrefix(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n + 1];  // Extra 0 at start
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];  // Cumulative
+        }
+        return prefix;
+    }
+
+    public static int rangeSum(int[] prefix, int left, int right) {
+        return prefix[right + 1] - prefix[left];  // Magic formula
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 10, 5, 15};
+        int[] prefix = buildPrefix(nums);  // [0,10,30,40,45,60]
+        System.out.println(rangeSum(prefix, 1, 3));  // 35 (20+10+5)
+    }
+}
+
