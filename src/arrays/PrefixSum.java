1c586b21a0aa68e384e18952d0b2f5274b9611fc
kenshiha <kyokasuigetsu979@gmail.com>
2025-12-14 19:14:21 +0530

    e. Two Pointers    int left = 0, right = arr.length-1;    while(left < right) { ... }
    2. Sliding Window
       for(int left=0; left<n; left++) {
         while(right < n && condition) right++;
         // window [left, right) is valid
       }
    
    3. Prefix Sum
       int[] prefix = new int[n+1];
       for(int i=1; i<=n; i++)
         prefix[i] = prefix[i-1] + arr[i-1];
    e. Two Pointers
       int left = 0, right = arr.length-1;
       while(left < right) { ... }
    
    2. Sliding Window
       for(int left=0; left<n; left++) {
         while(right < n && condition) right++;
         // window [left, right) is valid
       }
    
    3. Prefix Sum
       int[] prefix = new int[n+1];
       for(int i=1; i<=n; i++)
    	prefix[i] = prefix[i-1] + arr[i-1];
    feat: add prefix sum utility methods

--- src/arrays/PrefixSum.java
+++ src/arrays/PrefixSum.java
@@ -1,11 +1,13 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class PrefixSum {
     public static int[] buildPrefix(int[] nums) {
         int n = nums.length;
-        int[] prefix = new int[n + 1];  // Extra 0 at start
+        int[] prefix = new int[n+1];// Extra 0 at start
         for (int i = 1; i <= n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];  // Cumulative
+            prefix[i] = prefix[i - 1] + nums[i-1];  // Cumulative
         }
         return prefix;
     }
@@ -17,7 +19,8 @@ public class PrefixSum {
     public static void main(String[] args) {
         int[] nums = {10, 20, 10, 5, 15};
         int[] prefix = buildPrefix(nums);  // [0,10,30,40,45,60]
-        System.out.println(rangeSum(prefix, 1, 3));  // 35 (20+10+5)
+        System.out.println(rangeSum(prefix, 2, 3));// 35 (20+10+5)
+        System.out.println(Arrays.toString(prefix));
     }
 }
 
