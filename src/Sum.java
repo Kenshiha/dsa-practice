0510504c1f53ca46994ee7a7d1b56cb7690f69fe
kenshiha <kyokasuigetsu979@gmail.com>
2026-02-03 22:31:26 +0530

    just subtle

--- src/Sum.java
+++ src/Sum.java
@@ -18,7 +18,7 @@ public class Sum {
 
     public static void main(String[] args){
         int arr[] = {1,2,4,5,6,7,8};
-        int target = 9;
+        int target = 10;
 
         Sum s = new Sum();
         int[] result = s.twoSum(arr, target);
