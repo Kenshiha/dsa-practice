b1fa5122432d0382cf25382be68ce346e55090c4
kenshiha <kyokasuigetsu979@gmail.com>
2026-03-22 22:29:02 +0530

    minor changes

--- src/Sum.java
+++ src/Sum.java
@@ -3,34 +3,18 @@ import java.util.Map;
 
 public class Sum {
 
-//    public int[] twoSum(int[] arr,int target){
-//
-//        Map<Integer, Integer> map = new HashMap<>();
-//        for (int i = 0; i < arr.length;i++ ) {
-//            int need = target - arr[i];
-//            if (map.containsKey(need)) {
-//                return new int[]{map.get(need), i};
-//            }
-//        map.put(arr[i], i);
-//        }
-//        return new int[]{};
-//    }
-
-
-
     public int[] twoSum(int[] arr, int k){
+        Map<Integer, Integer> map = new HashMap<>();
 
-    Map<Integer, Integer> map= new HashMap<>();
-    for(int i =0 ; i <= arr.length; i++){
-        int ans = k - arr[i];
-        if(map.containsKey(ans)){
-            return new int[]{map.get(ans),i};
+        for(int i = 0; i <= arr.length; i++){
+            int ans = k - arr[i];
+            if(map.containsKey(ans)){
+                return new int[]{map.get(ans),i};
+            }
+            map.put(arr[i],i);
         }
-        map.put(arr[i],i );
-    }
         return new int[]{};
     }
-
     public static void main(String[] args){
         int arr[] = {1,2,4,5,6,7,8};
         int target = 10;
