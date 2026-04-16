ce7b3100a83028f9ba3e13a56bf38c3c311fcf71
kenshiha <kyokasuigetsu979@gmail.com>
2026-04-17 00:27:53 +0530

    minor changes

--- src/arrays/LongPrefix.java
+++ src/arrays/LongPrefix.java
@@ -1,4 +1,27 @@
 package arrays;
 
 public class LongPrefix {
+    public static String longestPrefix(String[] strs){
+        if (strs == null || strs.length == 0) return "";
+
+        String first = strs[0];
+
+        for(int i = 0; i < first.length(); i++){
+            char ch = first.charAt(i);
+
+            for(int j = 1; j < strs.length; j++){
+                if(i > strs[j].length() || strs[j].charAt(i) != ch){
+                    return first.substring(0,i);
+                }
+            }
+        }
+    return first;
+    }
+
+    public static void main(String[] args){
+
+        String[] strs = {"flock","flower","floeing"};
+        System.out.println(longestPrefix(strs));
+
+    }
 }
