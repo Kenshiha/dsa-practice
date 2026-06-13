0a67f512c96f858d0033647b52f09a6551b5bd9b
kenshiha <kyokasuigetsu979@gmail.com>
2026-06-13 22:20:00 +0530

    add line

--- src/test.java
+++ src/test.java
@@ -11,6 +11,27 @@ public class test {
         int c = a+b;
         return c;
     }
+
+    public int bst(int[] nums, int target){
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left <= right){
+
+            int ans = left + (right - left) / 2;
+
+            if(nums[ans] == target){
+                return ans;
+            } else if (nums[ans] < target) {
+                left = ans + 1;
+            }else {
+                right = ans - 1;
+            }
+        }
+
+        return -1;
+    }
     public static void main(String[] args){
 
         test obj = new test();
@@ -18,7 +39,7 @@ public class test {
         int d = obj.math(5,4);
         System.out.println(d);
         System.out.println("what");
-        System.out.println("nothing");
+
     }
 
 }
