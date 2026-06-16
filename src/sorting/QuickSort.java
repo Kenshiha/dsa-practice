2d2694a367187b6e76892fc72e55f21d13333957
kenshiha <kyokasuigetsu979@gmail.com>
2026-06-16 23:03:56 +0530

    insert function

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -12,23 +12,33 @@ public class QuickSort {
         quickSort(arr,finalPiviot+1,end);
       }
     }
-public static int partition(int[] arr,int start, int end){
+    public static int partition(int[] arr,int start, int end){
         int piviot = arr[end];
         int i = start - 1;
 
         for(int j = start; j < end; j++){
             if(arr[j] <= piviot){
                 i++;
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+//                int temp = arr[j];
+//                arr[j] = arr[i];
+//                arr[i] = temp;
+                swap(arr,i,j);
             }
         }
-        int temp = arr[i+1];
-        arr[i+1] = arr[end];
-        arr[end] = temp;
+//        int temp = arr[i+1];
+//        arr[i+1] = arr[end];
+//        arr[end] = temp;
+        swap(arr, i+1,end);
         return i+1;
-}
+    }
+
+    public static void swap(int[] arr, int i, int j){
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void main(String[] args){
         int[] arr = {11,2,23,10,5,8,15,9};
         System.out.println("Before Sorting = "+ Arrays.toString(arr));
